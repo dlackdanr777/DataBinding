de499d092e01fe7bd4d8b7d5e3d2d96b7c8abdb9
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2024-04-09 22:06:22 +0900

    [Upload] 기존 DataBind 업로드

---
+++ Assets/Scripts/DataBind/ButtonGetter.cs
@@ -0,0 +1,75 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Muks.DataBind
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonGetter : MonoBehaviour
+    {
+        [SerializeField] private string _dataID;
+        private Button _button;
+        private BindData<UnityAction> _data;
+        private UnityAction _action;
+
+
+        private bool _isAddListenerClear; //버튼에 함수가 등록됬나 안됬나 확인하는 함수
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+
+            if (string.IsNullOrEmpty(_dataID))
+            {
+                Debug.LogWarningFormat("Invalid text data ID. {0}", gameObject.name);
+                _dataID = gameObject.name;
+            }
+        }
+
+        private void Start()
+        {
+            Invoke("Enabled", 0.05f);
+        }
+
+
+        private void OnEnable()
+        {
+            Invoke("Enabled", 0.05f);
+        }
+
+
+        private void Enabled()
+        {
+            if (_isAddListenerClear)
+                return;
+
+            _data = DataBind.GetButtonValue(_dataID);
+
+            if(_data.Item == null)
+            {
+                Debug.LogError("넘겨받은 데이터가 존재하지 않습니다.");
+                return;
+            }
+
+            _action = _data.Item;
+            _button.onClick.AddListener(_action);
+            _isAddListenerClear = true;
+        }
+
+
+        /// <summary> 버튼 이벤트로 매개 변수로 넘긴 함수가 있는지 확인해 참, 거짓을 반환 하는 함수 </summary>
+        private bool IsListenerRegistered(UnityAction method)
+        {
+            var listeners = _button.onClick.GetPersistentEventCount();
+
+            for (int i = 0; i < listeners; i++)
+            {
+                if (_button.onClick.GetPersistentMethodName(i) == method.Method.Name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/DataBind/DataBind.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Muks.DataBind
+{
+    public static class DataBind
+    {
+        private static Dictionary<string, BindData<string>> _dataBindingText;
+
+        private static Dictionary<string, BindData<UnityAction>> _dataBindingUnityAction;
+
+        private static Dictionary<string, BindData<Sprite>> _dataBindingSprite;
+
+        private static Dictionary<string, BindData<object>> _dataBindingObject;
+
+        /// <summary>
+        /// 텍스트 데이터의 연결장소와 보낼 값을 지정해두는 함수(data값이 변할 경우 GetTextValue로 연결된 곳의 값도 변함)
+        /// </summary>
+        public static void SetTextValue(string dataID, string data)
+        {
+            if (_dataBindingText == null)
+                _dataBindingText = new Dictionary<string, BindData<string>>();
+
+            if (!_dataBindingText.TryGetValue(dataID, out BindData<string> textData))
+            {
+                textData = new BindData<string>();
+                _dataBindingText.Add(dataID, textData);
+            }
+            textData.Item = data;
+        }
+
+
+        /// <summary>
+        /// 저장된 텍스트 데이터의 연결장소를 불러와 연결하는 함수
+        /// </summary>
+        public static BindData<string> GetTextValue(string dataID)
+        {
+            if (_dataBindingText == null)
+                _dataBindingText = new Dictionary<string, BindData<string>>();
+
+            if (!_dataBindingText.TryGetValue(dataID, out BindData<string> textData))
+            {
+                textData = new BindData<string>();
+                _dataBindingText.Add(dataID, textData);
+            }
+            return textData;
+        }
+
+
+        /// <summary>
+        /// 버튼 데이터의 연결장소와 보낼값을 지정해두는 함수(action값이 변할 경우 GetButtonValue로 연결된 곳의 값도 변함)
+        /// </summary>
+        public static void SetButtonValue(string dataID, UnityAction action)
+        {
+            if (_dataBindingUnityAction == null)
+                _dataBindingUnityAction = new Dictionary<string, BindData<UnityAction>>();
+
+            if (!_dataBindingUnityAction.TryGetValue(dataID, out BindData<UnityAction> buttonData))
+            {
+                buttonData = new BindData<UnityAction>();
+                _dataBindingUnityAction.Add(dataID, buttonData);
+            }
+
+            buttonData.Item = action;
+        }
+
+
+        /// <summary>
+        /// 버튼 텍스트 데이터의 연결장소를 불러오는 함수
+        /// </summary>
+        public static BindData<UnityAction> GetButtonValue(string dataID)
+        {
+            if (_dataBindingUnityAction == null)
+                _dataBindingUnityAction = new Dictionary<string, BindData<UnityAction>>();
+
+            if (!_dataBindingUnityAction.TryGetValue(dataID, out BindData<UnityAction> buttonData))
+            {
+                buttonData = new BindData<UnityAction>();
+                _dataBindingUnityAction.Add(dataID, buttonData);
+            }
+            return buttonData;
+        }
+
+
+        /// <summary>
+        /// 버튼 데이터의 연결장소와 보낼값을 지정해두는 함수(action값이 변할 경우 GetButtonValue로 연결된 곳의 값도 변함)
+        /// </summary>
+        public static void SetSpriteValue(string dataID, Sprite sprite)
+        {
+            if (_dataBindingSprite == null)
+                _dataBindingSprite = new Dictionary<string, BindData<Sprite>>();
+
+            if (!_dataBindingSprite.TryGetValue(dataID, out BindData<Sprite> imageData))
+            {
+                imageData = new BindData<Sprite>();
+                _dataBindingSprite.Add(dataID, imageData);
+            }
+            imageData.Item = sprite;
+        }
+
+
+        /// <summary>dataID를 통해 이미지 데이터의 연결 장소를 불러오는 함수 </summary>
+        public static BindData<Sprite> GetSpriteValue(string dataID)
+        {
+            if (_dataBindingSprite == null)
+                _dataBindingSprite = new Dictionary<string, BindData<Sprite>>();
+
+            if (!_dataBindingSprite.TryGetValue(dataID, out BindData<Sprite> imageData))
+            {
+                imageData = new BindData<Sprite>();
+                _dataBindingSprite.Add(dataID, imageData);
+            }
+            return imageData;
+        }
+
+
+        /// <summary>dataID를 통해 UnityAction 함수를 반환하는 함수 </summary>
+        public static UnityAction GetAction(string dataID)
+        {
+            if (_dataBindingUnityAction == null)
+                _dataBindingUnityAction = new Dictionary<string, BindData<UnityAction>>();
+
+            if (!_dataBindingUnityAction.TryGetValue(dataID, out BindData<UnityAction> actionData))
+            {
+                actionData = new BindData<UnityAction>();
+                _dataBindingUnityAction.Add(dataID, actionData);
+            }
+            return actionData.Item;
+        }
+
+
+        /// <summary>dataID를 통해 오브젝트 자료형 데이터를 연결하는 함수 </summary>
+        public static void SetObjectValue(string dataID, object obj)
+        {
+            if (_dataBindingObject == null)
+                _dataBindingObject = new Dictionary<string, BindData<object>>();
+
+            if (!_dataBindingObject.TryGetValue(dataID, out BindData<object> objectData))
+            {
+                objectData = new BindData<object>();
+                _dataBindingObject.Add(dataID, objectData);
+            }
+            objectData.Item = obj;
+        }
+
+
+
+        /// <summary>dataID를 통해 연결된 오브젝트 자료형 데이터를 반환하는 함수 </summary>
+        public static object GetObjectValue(string dataID)
+        {
+            if (_dataBindingObject == null)
+                _dataBindingObject = new Dictionary<string, BindData<object>>();
+
+            if (!_dataBindingObject.TryGetValue(dataID, out BindData<object> objectData))
+            {
+                objectData = new BindData<object>();
+                _dataBindingObject.Add(dataID, objectData);
+            }
+
+            return objectData.Item;
+        }
+    }
+}
